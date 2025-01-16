daaf4c38b175471f539cf522329e31757ac1c816
kirdemorgan <drankov.alex@yandex.ru>
2025-01-16 17:58:45 +0500

    env fixes

--- src/main/java/xyz/demorgan/videoservice/config/MinioConfig.java
+++ src/main/java/xyz/demorgan/videoservice/config/MinioConfig.java
@@ -4,17 +4,26 @@ import io.minio.BucketExistsArgs;
 import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinioConfig {
+    @Value("${minio.endpoint}")
+    private String minioEndpoint;
+
+    @Value("${minio.root-user}")
+    private String minioUser;
+
+    @Value("${minio.root-password}")
+    private String minioPassword;
 
     @Bean
     public MinioClient minioClient() {
         return MinioClient.builder()
-                .endpoint("http://localhost:9000")
-                .credentials("demorgan", "demorgan")
+                .endpoint(minioEndpoint)
+                .credentials(minioUser, minioPassword)
                 .build();
     }
 
